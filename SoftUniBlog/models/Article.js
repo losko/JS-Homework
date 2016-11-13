7c81d85a583f9e2fb498ca352402dff08d839f9e
Losko <lozan_vlashev@abv.bg>
2016-11-13 17:01:14 +0200

    Work

---
+++ SoftUniBlog/models/Article.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+let articleSchema = mongoose.Schema({
+    title: { type: String, require: true },
+    content: { type: String, require: true },
+    author: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    date: { type: Date, default: Date.now() }
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
